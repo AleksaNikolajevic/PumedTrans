d6a96e76646242ba992c4afbcc2301648b9f0be6
AleksaNikolajevic <75322640+AleksaNikolajevic@users.noreply.github.com>
2024-03-05 11:39:07 +0100

    Initial Commit

---
+++ PumedTrans/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PumedTrans
+//
+//  Created by Aleksa Nikolajevic on 5.3.24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PumedTrans/PumedTransApp.swift
@@ -0,0 +1,17 @@
+//
+//  PumedTransApp.swift
+//  PumedTrans
+//
+//  Created by Aleksa Nikolajevic on 5.3.24..
+//
+
+import SwiftUI
+
+@main
+struct PumedTransApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
